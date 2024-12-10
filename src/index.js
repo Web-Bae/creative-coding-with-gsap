a823d800cc93a4e05d3923ace0474a06742c7516
Keegan Leary <leary.keegan@gmail.com>
2024-12-10 13:59:55 -0800

    completed marquee

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { Clock } from "./clock";
+import { Marquee } from "./marquee";
 
 import { reveal } from "./helpers/reveal";
 
 const clock = new Clock(".hero_clock");
+
+const marqueeElements = document.querySelectorAll(".marquee");
+marqueeElements.forEach(
+  (marqueeElement, index) => new Marquee(marqueeElement, index + 1 * 20)
+);
 reveal();
