bf3fc9c4fb7abf95512c4ed5533167c264540a8e
Keegan Leary <leary.keegan@gmail.com>
2024-12-10 17:52:27 -0800

    checkpoint-7

--- src/index.js
+++ src/index.js
@@ -3,20 +3,79 @@ import { Marquee } from "./marquee";
 import { Grid } from "./grid";
 import { Heading } from "./heading";
 import { ToolkitText } from "./toolkit";
+import { ColorModeButton } from "./colorModeButton";
 
 import { reveal } from "./helpers/reveal";
 
-const clock = new Clock(".hero_clock");
+class App {
+  constructor() {
+    this.clock = null;
+    this.marquees = [];
+    this.grid = null;
+    this.heading = null;
+    this.toolkit = null;
+    this.colorModeButton = null;
+  }
 
-const marqueeElements = document.querySelectorAll(".marquee");
-marqueeElements.forEach(
-  (marqueeElement, index) => new Marquee(marqueeElement, index + 1 * 20)
-);
-const grid = new Grid();
+  init() {
+    console.log("App Initializing...");
 
-document.fonts.ready.then(() => {
-  console.log("Fonts are loaded");
-  const heading = new Heading(document.querySelector(".hero_h1"));
-  const toolkit = new ToolkitText(".toolkit_p");
-  reveal();
-});
+    // Clock
+    this.clock = new Clock(".hero_clock");
+
+    // Grid
+    if (this.grid) this.grid.destroy();
+    this.grid = new Grid();
+
+    // Marquee
+    const marqueeElements = document.querySelectorAll(".marquee");
+    marqueeElements.forEach((marquee) =>
+      this.marquees.push(new Marquee(marquee))
+    );
+
+    // Heading
+    this.heading = new Heading(document.querySelector(".hero_h1"));
+
+    // ToolkitText
+    if (this.toolkit) this.toolkit.destroy();
+    this.toolkit = new ToolkitText(".toolkit_p");
+
+    // Initialize ColorModeButton and pass a reference to the handler
+    this.colorModeButton = new ColorModeButton(() =>
+      this.handleColorModeChange()
+    );
+
+    reveal();
+  }
+
+  handleColorModeChange() {
+    console.log("Color mode changed, reinitializing necessary components...");
+
+    // Update the grid
+    if (this.grid) {
+      this.grid.destroy();
+    }
+    this.grid = new Grid();
+
+    // Update the toolkit
+    if (this.toolkit) {
+      this.toolkit.destroy();
+    }
+    this.toolkit = new ToolkitText(".toolkit_p");
+
+    // Any other components that depend on color mode changes can also be updated here
+    console.log("Color mode-related reinitialization complete.");
+  }
+
+  listenToMotionPreferenceChanges() {}
+
+  start() {
+    document.fonts.ready.then(() => {
+      this.init();
+    });
+  }
+}
+
+// Instantiate and start the app
+const app = new App();
+app.start();
