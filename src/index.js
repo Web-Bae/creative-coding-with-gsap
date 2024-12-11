a8d2229edc13f7c1d1d401fc919ed42b1dafb9bd
Keegan Leary <leary.keegan@gmail.com>
2024-12-10 17:29:50 -0800

    toolkit tet animation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Clock } from "./clock";
 import { Marquee } from "./marquee";
 import { Grid } from "./grid";
 import { Heading } from "./heading";
+import { ToolkitText } from "./toolkit";
 
 import { reveal } from "./helpers/reveal";
 
@@ -16,5 +17,6 @@ const grid = new Grid();
 document.fonts.ready.then(() => {
   console.log("Fonts are loaded");
   const heading = new Heading(document.querySelector(".hero_h1"));
+  const toolkit = new ToolkitText(".toolkit_p");
+  reveal();
 });
-reveal();
