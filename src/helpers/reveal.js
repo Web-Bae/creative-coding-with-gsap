884ce680d39b3a084752070b67f763fa42cd5516
Keegan Leary <leary.keegan@gmail.com>
2024-12-12 13:53:02 -0800

    css loading screen

--- src/helpers/reveal.js
+++ src/helpers/reveal.js
@@ -1,8 +1,16 @@
 export function reveal() {
   const startHiddenAttributeName = "start-hidden";
+  const startRevealedAttributeName = "start-revealed";
   const elements = document.querySelectorAll(`[${startHiddenAttributeName}]`);
+  const elementsToHide = document.querySelectorAll(
+    `[${startRevealedAttributeName}]`
+  );
 
   elements.forEach((element) => {
     element.removeAttribute(startHiddenAttributeName);
+  });
+
+  elementsToHide.forEach((element) => {
+    element.remove();
   });
 }
